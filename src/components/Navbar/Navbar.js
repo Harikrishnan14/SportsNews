698b7863fa249bec6c35126aad8371ad5c96ac71
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-03-11 16:14:56 +0530

    Responsive Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import Lens from '../../Assets/imgs/LensVector.png'
+import BurgerMenu from '../../Assets/imgs/BurgerMenu.png'
+import CloseMenu from '../../Assets/imgs/CloseMenu.png'
 
 const Navbar = () => {
+
+    const [toggleBurgerMenu, setToggleBurgerMenu] = useState(false)
+
     return (
         <nav className='Navbar'>
-            <div>
+            <div className='logo-div'>
                 <div className="logo">Sport News</div>
             </div>
-            <div>
+            <div className='nav-div'>
                 <ul className='nav-list'>
                     <li className='list-item' style={{ fontWeight: "700", color: "#262626" }}>Home</li>
                     <li className='list-item'>Category</li>
@@ -18,10 +23,24 @@ const Navbar = () => {
                     <li className='list-item'>Sports Article</li>
                 </ul>
             </div>
-            <div>
+            <div className='search-button-div'>
                 <button className='search-btn'>
                     <img className='lens-img' src={Lens} alt='' />Search
                 </button>
+            </div>
+            <div className="burger-menu-div">
+                {!toggleBurgerMenu && <img src={BurgerMenu} alt="" className='burger-menu-icon' onClick={() => setToggleBurgerMenu(true)} />}
+                {toggleBurgerMenu && <img src={CloseMenu} alt="" className='burger-menu-icon' onClick={() => setToggleBurgerMenu(false)} />}
+                {toggleBurgerMenu && <div className="burger-menu">
+                    <ul className='burger-menu-nav-list'>
+                        <li className='burger-menu-list-item'>Home</li>
+                        <li className='burger-menu-list-item'>Category</li>
+                        <li className='burger-menu-list-item'>Trending News</li>
+                        <li className='burger-menu-list-item'>Recent News</li>
+                        <li className='burger-menu-list-item'>Clubs Ranking</li>
+                        <li className='burger-menu-list-item'>Sports Article</li>
+                    </ul>
+                </div>}
             </div>
         </nav>
     )
